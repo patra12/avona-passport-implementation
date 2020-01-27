0e820306bee5256ed46d4792a3d815af21f6a3fd
git-testa <gesemiy485@repshop.net>
2020-01-27 19:02:21 +0530

    just keep a copey of the basic statergy code for leter referance

---
+++ server/passportbasic.js
@@ -0,0 +1,60 @@
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , BasicStrategy = require('passport-http').BasicStrategy
+  , morgan  = require('morgan')
+  , app     = express();
+ 
+ 
+app.use(morgan());
+app.use(passport.initialize());
+ 
+// curl -v -I http://127.0.0.1:3000/
+// curl -v -I --user bob:secret http://127.0.0.1:3000/
+app.get('/',
+    // Authenticate using HTTP Basic credentials, with session support disabled.
+    passport.authenticate('basic', { session: false }),
+    function(req, res){
+        res.json({ username: req.user.username, email: req.user.email });
+    });
+ 
+ 
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+ 
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+ 
+// Use the BasicStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.
+passport.use(new BasicStrategy({
+  },
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+       
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure.  Otherwise, return the authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (user.password != password) { return done(null, false); }
+        return done(null, user);
+      })
+    });
+  }
+));
+ 
+app.listen(3000);
